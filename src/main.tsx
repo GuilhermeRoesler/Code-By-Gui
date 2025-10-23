6ef7875dcc290ecf646d65c5607badfb607391bf
[dyad] <guiroesler2@gmail.com>
2025-10-23 19:05:16 -0300

    Reverted all changes back to version f5d7faf5f6a673fa0efda358a9f095db87ccd5d1

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,6 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import "./App.css";
-import "./styles/animations.css";
-import "./styles/components.css";
+import "./App.css"
 
 createRoot(document.getElementById("root")!).render(<App />);
