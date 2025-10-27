895b51c1769e0662a4cf3e85663c01531d11d4f6
[dyad] <guiroesler2@gmail.com>
2025-10-27 08:28:30 -0300

    [dyad] Refactored CSS into modular files - wrote 3 file(s), deleted 1 file(s) + extra files edited outside of Dyad

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import "./App.css"
+import "./styles/animations.css";
+import "./styles/components.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
