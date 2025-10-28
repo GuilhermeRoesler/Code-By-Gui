a71b4393490730c0ad3e4c518d09dcea4dbd6759
[dyad] <guiroesler2@gmail.com>
2025-10-28 10:09:19 -0300

    [dyad] Refactor CSS into modular files - wrote 11 file(s), deleted 1 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,16 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import "./styles/animations.css";
-import "./styles/components.css";
 import "./styles/carousel3d.css";
+import "./styles/glow-card.css";
+import "./styles/glow-hover-button.css";
+import "./styles/golden-shock-card.css";
+import "./styles/grid-hover-button.css";
+import "./styles/halftone-image.css";
+import "./styles/hero.css";
+import "./styles/preloader.css";
+import "./styles/scrollbar.css";
+import "./styles/skill-card.css";
+import "./styles/view-switcher.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
